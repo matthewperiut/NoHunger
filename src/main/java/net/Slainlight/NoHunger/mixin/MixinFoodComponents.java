57025d2d0dbb39359da6c84ec6840d648b351471
sink666 <er.basilisk@gmail.com>
2023-02-18 13:10:17 +0000

    finish food changes; chicken, pufferfish, rename Rotten Flesh handler

--- src/main/java/net/Slainlight/NoHunger/mixin/MixinFoodComponents.java
+++ src/main/java/net/Slainlight/NoHunger/mixin/MixinFoodComponents.java
@@ -1,5 +1,6 @@
 package net.Slainlight.NoHunger.mixin;
 
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponents;
 import org.objectweb.asm.Opcodes;
@@ -15,12 +16,43 @@ public class MixinFoodComponents {
             method = "<clinit>",
             slice = @Slice(
                     from = @At(value = "FIELD", opcode = Opcodes.PUTSTATIC, target = "Lnet/minecraft/item/FoodComponents;RABBIT_STEW:Lnet/minecraft/item/FoodComponent;"),
-                    to = @At(value = "FIELD", opcode = Opcodes.PUTSTATIC, target = "Lnet/minecraft/item/FoodComponents;ROTTEN_FLESH:Lnet/minecraft/item/FoodComponent;")),
+                    to = @At(value = "FIELD", opcode = Opcodes.PUTSTATIC, target = "Lnet/minecraft/item/FoodComponents;ROTTEN_FLESH:Lnet/minecraft/item/FoodComponent;")
+            ),
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/effect/StatusEffectInstance;<init>(Lnet/minecraft/entity/effect/StatusEffect;II)V"),
             allow = 1
     )
-    private static void NoHunger$rottenFleshCausesHunger(Args args) {
+    private static void NoHunger$rottenFleshCausesPoison(Args args) {
         args.set(0, StatusEffects.POISON);
         args.set(1, 150);
+    }
+
+    @ModifyArgs(
+            method = "<clinit>",
+            slice = @Slice(
+                    from = @At(value = "FIELD", opcode = Opcodes.PUTSTATIC, target = "Lnet/minecraft/item/FoodComponents;POTATO:Lnet/minecraft/item/FoodComponent;"),
+                    to = @At(value = "FIELD", opcode = Opcodes.PUTSTATIC, target = "Lnet/minecraft/item/FoodComponents;PUFFERFISH:Lnet/minecraft/item/FoodComponent;")
+            ),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/effect/StatusEffectInstance;<init>(Lnet/minecraft/entity/effect/StatusEffect;II)V"),
+            allow = 3
+    )
+    private static void NoHunger$pufferfishCausesWeakness(Args args) {
+        StatusEffect StatusArg = args.get(0);
+        if (StatusArg == StatusEffects.HUNGER) {
+            args.set(0, StatusEffects.WEAKNESS);
+            args.set(1, 150);
+        }
+    }
+
+    @ModifyArgs(
+            method = "<clinit>",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/effect/StatusEffectInstance;<init>(Lnet/minecraft/entity/effect/StatusEffect;II)V"),
+            allow = 1
+    )
+    private static void NoHunger$rawChickenCausesPoison(Args args) {
+        StatusEffect StatusArg = args.get(0);
+        if (StatusArg == StatusEffects.HUNGER) {
+            args.set(0, StatusEffects.POISON);
+            args.set(1, 150);
+        }
     }
 }
